9b01a042aa5617a6d7ea958a6af3bf126b2af632
Mohd Abdul Mannan <ma24042000@gmail.com>
2023-09-30 14:29:20 +0530

    import and export in node

---
+++ exporting.js
@@ -0,0 +1,16 @@
+//exporting a single variable
+// var a = 10;
+// module.exports = a;
+
+//exporting multiple variables
+// let a = 10;
+// let b = 20;
+// let c = 30;
+//module.exports = {a,b,c};
+
+// exporting a function
+// const a = (val) => val *10;
+// module.exports = a;
+/*
+    this is a comment for multiple lines of code 
+*/
---
+++ start.js
@@ -0,0 +1,24 @@
+// console.log("Starting BackEnd with Node.js and Express.js");
+// console.log("nodejs is c++ code taken from chrome browser v8 engine, wrapped in js code");
+// console.log("js->Node->c++");
+//learning importing and exporting
+
+// importing a single variable
+// const a = require("./exporting");
+// console.log(a);
+
+//Node js is a run-time environment 
+
+// importing multiple variables with
+// const a = require("./exporting");
+// console.log(a.a, a.b, a.c);
+
+//importing functions from different file or folders just the difference is that
+// we need to import using "." to go to folder for current folder it is './' 
+// to go back further we need to write that '../' and so on 
+
+//importing functions
+// const a = require("./exporting");
+// console.log(a(10));
+// here is a function imported from exporting file 
+
