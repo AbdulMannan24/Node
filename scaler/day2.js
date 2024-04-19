6e8c93a39ed95fec7eee5c6ad72ef2c2b8be968c
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-04-19 08:20:38 +0530

    day 2

---
+++ scaler/day2.js
@@ -0,0 +1,9 @@
+
+/* NODE Module System
+    Global and Window object
+    Global is Node.js, window is for browser
+
+    The variables/functions created in limited to that particular file/module only
+    use export syntax to use them in other files with module.exports 
+*/
+
